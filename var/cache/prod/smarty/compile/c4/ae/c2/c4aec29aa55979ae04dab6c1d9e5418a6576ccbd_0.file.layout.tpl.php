56fb4e8d50338e7198dd9728eb168d79da72c7d3
Carlosmgp4 <carlosmanuelgamezperezdeguzman@gmail.com>
2021-11-25 17:26:59 +0100

    resubida

---
+++ var/cache/prod/smarty/compile/c4/ae/c2/c4aec29aa55979ae04dab6c1d9e5418a6576ccbd_0.file.layout.tpl.php
@@ -0,0 +1,31 @@
+<?php
+/* Smarty version 3.1.39, created on 2021-11-24 19:11:17
+  from '/var/www/prestashop/admin646peuwfz/themes/default/template/controllers/login/layout.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.39',
+  'unifunc' => 'content_619e8045a7b4e9_37870239',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'c4aec29aa55979ae04dab6c1d9e5418a6576ccbd' => 
+    array (
+      0 => '/var/www/prestashop/admin646peuwfz/themes/default/template/controllers/login/layout.tpl',
+      1 => 1633363913,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_619e8045a7b4e9_37870239 (Smarty_Internal_Template $_smarty_tpl) {
+echo $_smarty_tpl->tpl_vars['header']->value;?>
+
+<?php echo $_smarty_tpl->tpl_vars['page']->value;?>
+
+<?php echo $_smarty_tpl->tpl_vars['footer']->value;?>
+
+<?php }
+}
